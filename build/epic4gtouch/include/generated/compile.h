bea87ff1b640eced4322d47ef02ff312c2262a4e
Dean Dubois <dubois.1996@gmail.com>
2011-12-21 20:52:20 -0500

    Please do not use this kernel source yet.... It causes Sleep of Death...

---
+++ build/epic4gtouch/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Dec 21 19:43:26 EST 2011"
+#define LINUX_COMPILE_TIME "19:43:26"
+#define LINUX_COMPILE_BY "dean"
+#define LINUX_COMPILE_HOST "dean-desktop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
